4ad10c0fe877af3252c7a7fef7935c46d54078db
Andreina2 <andreinaarias5@gmail.com>
2025-01-09 22:35:03 +0100

    create branch dev

---
+++ src/main/java/dev/andreina/springboot/controller/HomeController.java
@@ -0,0 +1,25 @@
+//package dev.andreina.springboot.controller;
+
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+
+//@RestController
+//public package dev.andreina.springboot.controller;
+
+//public class HomeController {
+
+    //Get
+   // @GetMapping("/")
+    //public String getMethodName() {
+    //    return new String("Hello Spring Boot");
+   // }
+    
+
+//}
+
+
+
+
+
+
